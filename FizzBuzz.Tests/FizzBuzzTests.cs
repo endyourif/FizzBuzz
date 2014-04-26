ba4f7b71e96774846cc2da74a1d985abcb929c62
Jamie Munro <jmunro@fusebill.com>
2014-04-26 09:48:25 -0400

    initial commit

---
+++ FizzBuzz.Tests/FizzBuzzTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz.Tests
+{
+    [TestClass]
+    public class FizzBuzzTests
+    {
+        private FizzBuzz _fizzBuzz;
+
+        public FizzBuzzTests()
+        {
+            _fizzBuzz = new FizzBuzz();
+        }
+
+        [TestMethod]
+        public void Given1Expect1()
+        {
+            const int expected = 1;
+            var actual = _fizzBuzz.Calculate(1);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Given3ExpectFizz()
+        {
+            const string expected = "Fizz";
+            var actual = _fizzBuzz.Calculate(3);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Given5ExpectBuzz()
+        {
+            const string expected = "Buzz";
+            var actual = _fizzBuzz.Calculate(5);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Given15Expect_FizzBuzz()
+        {
+            const string expected = "FizzBuzz";
+            var actual = _fizzBuzz.Calculate(15);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void Given0ExpectException()
+        {
+            _fizzBuzz.Calculate(0);
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public object Calculate(int p)
+        {
+            if (p < 1)
+                throw new IndexOutOfRangeException();
+            if (p % 15 == 0)
+                return "FizzBuzz";
+            if (p % 3 == 0)
+                return "Fizz";
+            if (p % 5 == 0)
+                return "Buzz";
+
+            return p;
+        }
+    }
+}
